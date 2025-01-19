56aef5278a343edaa0c02ba2f2c590ceb162d721
Tri Nguyen <6278989+5c0r@users.noreply.github.com>
2025-01-19 15:49:40 +0200

    feat: show date on hovering since

--- src/components/PostListItem.svelte
+++ src/components/PostListItem.svelte
@@ -7,7 +7,7 @@
 
 <li>
     <a href={post.url}>{post.title} <span class="origin">{post.origin ? `(${post.origin})` : ""}</span></a>
-    <p class="subtitle">{post.score} points by {post.author?.username ?? 'Unknown user'} {post.since} | <a href="/discussion/{post.id}" target="_self">{post.reply_count} comments</a></p>
+    <p class="subtitle">{post.score} points by {post.author?.username ?? 'Unknown user'} <span title={new Date(post.create_at).toString()}>{post.since}</span> | <a href="/discussion/{post.id}" target="_self">{post.reply_count} comments</a></p>
 </li>
 
 <style>
